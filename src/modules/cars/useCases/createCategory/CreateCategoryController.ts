855c10d1204890ed7b3692639551f162be1eb881
edubarr <eduardobarros@edge.ufal.br>
2023-03-17 23:25:28 -0300

    Refatorado listagem de categorias

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+class CreateCategoryController {
+  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
+  handler(req: Request, res: Response): Response {
+    const { name, description } = req.body;
+
+    this.createCategoryUseCase.execute({ name, description });
+
+    return res.status(201).send();
+  }
+}
+
+export { CreateCategoryController };
---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+class ListCategoriesController {
+  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
+
+  handler(req: Request, res: Response): Response {
+    const all = this.listCategoriesUseCase.execute();
+
+    return res.json(all);
+  }
+}
+
+export { ListCategoriesController };
