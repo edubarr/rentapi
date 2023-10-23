b516e623dda53f64ed86b80eaa6b9a7e9cb054db
edubarr <eduaraujobarros@gmail.com>
2023-10-23 17:43:21 -0300

    feat: add docker pg db

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+
+import { AppDataSource } from "./database";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
+
+AppDataSource.initialize()
+  .then(() => {
+    // here you can start to work with your database
+  })
+  .catch((error) => console.log(error));
 
 const app = express();
 
